d25a36aad264941e4482fa3bfd8aadd97fb0a269
jose alcantara <jose_luis967@hotmail.com>
2022-05-12 16:50:00 -0400

    Se actualiza formulario con validaciones

--- js/main.js
+++ js/main.js
@@ -1,140 +1,20 @@
-$(document).ready(function(){
-//validate.js para validar el formulario
-var constraints = {
-    nombre: {
-        presence: true,
-        //it must be between 3 and 20 characters long
-        length: {
-        minimum: 3,
-        maximum: 20
-        },
-        format: {
-        // We don't allow anything that a-z and 0-9
-        pattern: "[a-z]+",
-        // but we don't care if the username is uppercase or lowercase
-        flags: "i",
-        message: "Debe ser un nombre real"
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+$(document).ready(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
         }
-    },
-    email: {
-        presence: true,
-        email: true
-    },
-    rut: {
 
-        presence: true,
-
-    },
-    City: {
-
-        presence: true,
-
-    },
-    textarea: {
-        presence: true,
-        length: {
-            minimum: 50
-        }
-    }
-    };
-
-    var form = document.querySelector("form");
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      handleFormSubmit(form);
-    });
-
-    // Hook up the inputs to validate on the fly
-    var inputs = document.querySelectorAll("input, textarea, select")
-    for (var i = 0; i < inputs.length; ++i) {
-      inputs.item(i).addEventListener("change", function(ev) {
-        var errors = validate(form, constraints) || {};
-        showErrorsForInput(this, errors[this.name])
-      });
-    }
-
-    function handleFormSubmit(form, input) {
-      // validate the form against the constraints
-      var errors = validate(form, constraints);
-      // then we update the form to reflect the results
-      showErrors(form, errors || {});
-      if (!errors) {
-        showSuccess();
-      }
-    }
-
-    // Updates the inputs with the validation errors
-    function showErrors(form, errors) {
-      // We loop through all the inputs and show the errors for that input
-      _.each(form.querySelectorAll("input[name], select[name]"), function(input) {
-        // Since the errors can be null if no errors were found we need to handle
-        // that
-        showErrorsForInput(input, errors && errors[input.name]);
-      });
-    }
-
-    // Shows the errors for a specific input
-    function showErrorsForInput(input, errors) {
-      // This is the root of the input
-      var formGroup = closestParent(input.parentNode, "form-group")
-        // Find where the error messages will be insert into
-        , messages = formGroup.querySelector(".messages");
-      // First we remove any old messages and resets the classes
-      resetFormGroup(formGroup);
-      // If we have errors
-      if (errors) {
-        // we first mark the group has having errors
-        formGroup.classList.add("has-error");
-        // then we append all the errors
-        _.each(errors, function(error) {
-          addError(messages, error);
-        });
-      } else {
-        // otherwise we simply mark it as success
-        formGroup.classList.add("has-success");
-      }
-    }
-
-    // Recusively finds the closest parent that has the specified class
-    function closestParent(child, className) {
-      if (!child || child == document) {
-        return null;
-      }
-      if (child.classList.contains(className)) {
-        return child;
-      } else {
-        return closestParent(child.parentNode, className);
-      }
-    }
-
-    function resetFormGroup(formGroup) {
-      // Remove the success and error classes
-      formGroup.classList.remove("has-error");
-      formGroup.classList.remove("has-success");
-      // and remove any old messages
-      _.each(formGroup.querySelectorAll(".help-block.error"), function(el) {
-        el.parentNode.removeChild(el);
-      });
-    }
-
-    // Adds the specified error with the following markup
-    // <p class="help-block error">[message]</p>
-    function addError(messages, error) {
-      var block = document.createElement("p");
-      block.classList.add("help-block");
-      block.classList.add("error");
-      block.innerText = error;
-      console.log(block)
-      console.log(messages)
-
-      messages.appendChild(block);
-    }
-
-    function showSuccess() {
-      // We made it \:D/
-      alert("Success!");
-    }
-
+        form.classList.add('was-validated')
+      }, false)
+    })
 });
-
-
